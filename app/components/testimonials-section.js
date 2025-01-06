cde20eaa0b4405d42bc266858ee1f450d1fc6176
Sam Bordo <sambordo1@gmail.com>
2025-01-05 20:55:23 -0500

    add testimonials

--- app/components/testimonials-section.js
+++ app/components/testimonials-section.js
@@ -3,17 +3,18 @@ import Component from '@ember/component';
 export default Component.extend({
   testimonials: Object.freeze([
     {
+      name: 'Anthony Lukasavage',
+      text: "I've had the pleasure of managing and mentoring Sam for the last 3 years. Sam joined my team as an intern on the Software Engineering Team and quickly rose to become an indispensable contributor. Not long into his internship, it was obvious Sam was a rising star, eager to take on any challenge. Undeterred by unfamiliar technologies (e.g., AWS, node, ember.js, Potsgres) and processes (e.g., Jira, Gitlab, Zoho), Sam quickly took on a deep understanding of Innovu's products and the underlying tech stack. Sam absorbed all the knowledge I imparted like a sponge, but also sought out more like a shark, often bringing his own innovative solutions to the team. Showing a keen interest in ensuring the reliability of our systems, Sam took on the critical role of managing QA for Innovu's SaaS application, Lens. In this capacity, Sam operated as a force multiplier for the entire Software Engineering Team through his meticulous testing strategy, inter-team collaboration, and ability to consistently deliver clear, actionable information to engineers and stakeholders. Even while early in his career, Sam stood toe to toe with veteran engineers in terms of both the impact he had and the respect he duly earned. I would hire Sam again at the earliest opportunity. He truly embodies the core attributes of Innovu's engineers with his sense of ownership, excellence in performance, and humility. Any engineering team would be lucky to have him.",
+      imageUrl: 'tony.png',
+      link: 'https://www.linkedin.com/in/samuel-bordo-825688174/details/recommendations/',
+    },
+    {
       name: 'Daniel Humenik',
       text: "Sam interned with the IT team at Innovu during COVID-19 in the summer of 2020. Due to Coronavirus, Sam worked remotely, however this did not inhibit his desire and passion to jump right in and learn. Sam quickly learned how to setup a LAMP stack. Sam used that knowledge to to research and improve our asset management system. Sam deployed a new asset management system locally, tested the functionality and demonstrated the product to the IT team. This system was adopted by the IT due to Sam's work. Sam continued to demonstrate his abilities to learn quickly by using Perl and Python to develop scripts that quickly reconciled a rather time-intensive process. Sam was an absolute pleasure to work with during his internship at Innovu. Sam displayed great time management, passion and desire to learn, and his ability to work independently and as a team. I know Sam will be an industry leader in a few short years!",
       imageUrl: 'dan.png',
       link: 'https://www.linkedin.com/in/samuel-bordo-825688174/details/recommendations/',
     },
-    // {
-    //   name: 'Anthony Lukasavage',
-    //   text: 'Coming soon',
-    //   imageUrl: 'tony.png',
-    //   link: 'https://www.linkedin.com/in/samuel-bordo-825688174/details/recommendations/',
-    // },
-    // Add as many testimonials as you like
   ]),
 });
+
+
